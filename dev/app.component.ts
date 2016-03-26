71596a0ccf2651541232f38b7c27b99d7356ba6b
Rich Brewer <rbrewer101@gmail.com>
2016-03-25 17:43:52 -0700

    First changes.

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,11 +1,15 @@
 import {Component} from 'angular2/core';
+import {MyComponentComponent} from './my-component.component';
+
 
 @Component({
     selector: 'my-app',
     template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
+        <h3>Angular 2 App (using AppComponent2)</h3>
+        <h3>Here is the second component:</h3>
+        <my-component></my-component>
     `,
+    directives: [MyComponentComponent]
 })
 export class AppComponent {
 
---
+++ dev/my-component.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-component',
+    template: `
+    <h3>This is my component.</h3>
+    `,
+})
+
+export class MyComponentComponent {
+
+}
